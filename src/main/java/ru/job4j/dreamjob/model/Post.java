582d7844a5bedaacd30c1f73a0aacfa34e283b56
fourbarman <maks.java@yandex.ru>
2022-03-06 22:18:22 +0400

    DreamJob. Bootstrap. Thymeleaf. Loop.

---
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -0,0 +1,66 @@
+package ru.job4j.dreamjob.model;
+
+import java.util.Objects;
+
+/**
+ * Post
+ * model.
+ */
+public class Post {
+    private int id;
+    private String name;
+    private String description;
+    private String created;
+
+    public Post(int id, String name, String description, String created) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.created = created;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+}
