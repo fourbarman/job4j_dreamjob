3922e171688903fba0116a511e57a62305512e20
fourbarman <maks.java@yandex.ru>
2022-03-07 15:07:15 +0400

    DreamJob. Add candidate form added.

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -3,8 +3,8 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.store.CandidateStore;
-import ru.job4j.dreamjob.store.PostStore;
 
 @Controller
 public class CandidateController {
@@ -14,5 +14,11 @@ public class CandidateController {
     public String posts(Model model) {
         model.addAttribute("candidates", candidateStore.findAll());
         return "candidates";
+    }
+
+    @GetMapping("/addCandidate")
+    public String addCandidate(Model model) {
+        model.addAttribute("candidate", new Candidate(0, "Заполните поле", "Описание", "Дата"));
+        return "addCandidate";
     }
 }
