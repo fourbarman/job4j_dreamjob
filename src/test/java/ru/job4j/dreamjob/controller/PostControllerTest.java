ff32b30ee4f60d520d90843c537deb3d498d7344
fourbarman <maks.java@yandex.ru>
2022-05-12 17:22:24 +0400

    Post controller tests added.

--- src/test/java/ru/job4j/dreamjob/controller/PostControllerTest.java
+++ src/test/java/ru/job4j/dreamjob/controller/PostControllerTest.java
@@ -23,6 +23,9 @@ import static org.mockito.Mockito.*;
  * @since 11.05.2022.
  */
 public class PostControllerTest {
+    /**
+     * Test posts.
+     */
     @Test
     public void whenPosts() {
         List<Post> posts = Arrays.asList(
@@ -42,6 +45,9 @@ public class PostControllerTest {
         assertThat(page, is("posts"));
     }
 
+    /**
+     * Test createPost.
+     */
     @Test
     public void whenCreatePost() {
         Post input = new Post(1, "New post1", "New post1 desc", "2021-11-11 11:11:11", new City(1, "CityName1"), true);
@@ -56,16 +62,85 @@ public class PostControllerTest {
         assertThat(page, is("redirect:/posts"));
     }
 
+    /**
+     * Test addPost.
+     */
     @Test
     public void whenAddPost() {
-
+        List<City> cities = Arrays.asList(
+                new City(1, "CityName1"),
+                new City(2, "CityName2")
+        );
+        Model model = mock(Model.class);
+        PostService postService = mock(PostService.class);
+        CityService cityService = mock(CityService.class);
+        PostController postController = new PostController(
+                postService,
+                cityService
+        );
+        when(cityService.getAllCities()).thenReturn(cities);
+        String page = postController.addPost(model, new MockHttpSession());
+        verify(model).addAttribute("cities", cities);
+        assertThat(page, is("addPost"));
     }
-    @Test
-    public void whenUpdatePost() {
 
-    }
+    /**
+     * Test savePost.
+     */
     @Test
     public void whenSavePost() {
+        Post input = new Post(1, "New post1", "New post1 desc", "2021-11-11 11:11:11", new City(1, "CityName1"), true);
+        PostService postService = mock(PostService.class);
+        CityService cityService = mock(CityService.class);
+        PostController postController = new PostController(
+                postService,
+                cityService
+        );
+        String page = postController.savePost(input);
+        verify(postService).add(input);
+        assertThat(page, is("redirect:/posts"));
+    }
 
+    /**
+     * Test updatePost.
+     */
+    @Test
+    public void whenUpdatePost() {
+        Post input = new Post(1, "New post1", "New post1 desc", "2021-11-11 11:11:11", new City(1, "CityName1"), true);
+        PostService postService = mock(PostService.class);
+        CityService cityService = mock(CityService.class);
+        PostController postController = new PostController(
+                postService,
+                cityService
+        );
+        String page = postController.updatePost(input);
+        verify(postService).update(input);
+        assertThat(page, is("redirect:/posts"));
+    }
+
+    /**
+     * Test formUpdatePost.
+     */
+    @Test
+    public void whenFormUpdatePost() {
+        Post input = new Post(1, "New post1", "New post1 desc", "2021-11-11 11:11:11", new City(1, "CityName1"), true);
+        int id = input.getId();
+        List<City> cities = Arrays.asList(
+                new City(1, "CityName1"),
+                new City(2, "CityName2")
+        );
+        Model model = mock(Model.class);
+        PostService postService = mock(PostService.class);
+        CityService cityService = mock(CityService.class);
+        PostController postController = new PostController(
+                postService,
+                cityService
+        );
+        when(postService.findById(id)).thenReturn(input);
+        when(cityService.getAllCities()).thenReturn(cities);
+        String page = postController.formUpdatePost(model, id);
+        verify(model).addAttribute("post", input);
+        verify(model).addAttribute("cities", cities);
+        assertThat(page, is("updatePost"));
     }
 }
